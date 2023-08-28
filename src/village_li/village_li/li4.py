e1ea08723607e48af9fdb17ceae0fb555db22fda
zwc <bitzwc@sina.cn>
2023-08-28 08:18:47 +0800

    增加李三借钱和王二看小说的代码

--- src/village_li/village_li/li4.py
+++ src/village_li/village_li/li4.py
@@ -3,7 +3,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from std_msgs.msg import UInt32
-
+from village_interfaces.srv import BorrowMoney
 
 class WriterNode(Node):
     def __init__(self, name):
@@ -14,9 +14,14 @@ class WriterNode(Node):
         self.i = 0  # i 是个计数器，用来算章节编号的
         #计时器，按这个时间来发布
         self.create_timer(timer_period_sec=time_period, callback=self.time_callback)
-
-        self.account = 0
+        self.account = 100
         self.create_subscription(UInt32, 'sexy_girl_money', self.recv_money_callback, 10)
+
+        #创建服务，在节点初始化时完成
+        #参数：服务接口类型
+             #服务名称：全局唯一
+             #回调函数：服务端处理逻辑
+        self.borrow_server = self.create_service(BorrowMoney, "borrow_money", self.borrow_money_callback)
 
     # 发布“小说”
     def time_callback(self):
@@ -31,6 +36,20 @@ class WriterNode(Node):
         self.account += money.data
         self.get_logger().info('已经收到稿费 : %d' % self.account)
 
+    # 对外借钱 
+    # 实现create_service里的callback方法，固定参数顺序，必须是request和response输入，response输出，request和response的数据结构参考srv里定义的输入和输出
+    def borrow_money_callback(self, request, response):
+        # 借钱超过账户的10%，借钱失败，返回0
+        if request.money <= int(self.account * 0.1):
+            self.account -= request.money
+            response.money = request.money
+            response.success = True
+            self.get_logger().info('借钱成功， 借出 %d，余额 %d' %(request.money, self.account))
+        else:
+            response.money = 0
+            response.success = False
+            self.get_logger().info('借钱失败， 余额 %d 不足，借钱 %d 失败' %(self.account, request.money))
+        return response
 
 
 def main(args=None):
--- src/village_wang/src/wang2.cpp
+++ src/village_wang/src/wang2.cpp
@@ -1,15 +1,73 @@
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/u_int32.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "village_interfaces/srv/sell_novel.hpp"
+#include <queue>
+
+
+//占位符，占位回调函数的参数个数
+using std::placeholders::_1; //?
+using std::placeholders::_2;
+
 
 class SingleDogNode : public rclcpp::Node{
 public:
+    // 构造函数,有一个参数为节点名称，实现Node初始化，将name传入
     SingleDogNode(std::string name):Node(name){
         RCLCPP_INFO(this->get_logger(), "大家好，我是" + name);
-    };
-    //todo:订阅“小说”
 
-    //todo:发布“小说付费”
+        //创建一个订阅者来订阅李四写的小说，通过名字sexy_girl
+        //std::bind和std::function结合实现回调技术，因为topic_callback是成员函数，需要有对象来调用，这里直接用
+        //_1是占位符，表示topic_callback函数的参数只有一个
+        sub_novel = this->create_subscription<std_msgs::msg::String>("sexy_girl", 10, std::bind(&SingleDogNode::topic_callback, this, _1));
+
+        //创建发布者
+        pub_money = this->create_publisher<std_msgs::msg::UInt32>("sexy_girl_money", 10);
+
+        //创建回调函数组对象
+        callback_group_service_ = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+
+        //创建服务端，用于提供买书服务
+        server_ = this->create_service<village_interface::srv::SellNovel>(
+            "sell_novel",                             
+            std::bind(&SingleDogNode::sell_book_callback,this,_1,_2),
+            
+        );
+    };
 
 private:
+    //声明一个订阅者（成员变量）,用于订阅小说
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_novel;
+
+    //声明一个发布者（成员变量）,用于给李四钱
+    rclcpp::Publisher<std_msgs::msg::UInt32>::SharedPtr pub_money;
+
+    //声明一个小说章节队列
+    std::queue<std::string>  novels_queue;
+
+    //声明一个服务回调组，解决卖书回调和接收小说回调互相等待出现死锁
+    rclcpp::CallbackGroup::SharedPtr callback_group_service_;
+
+    //声明一个服务端
+    rclcpp::Service<village_interfaces::srv::SellNovel>::SharedPtr server_;
+
+    //声明话题数据的回调函数，看到小说后，才给李四打钱，打钱由小说topic的消息来触发
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg){
+        //新建一张人民币
+        std_msgs::msg::UInt32 money;
+        money.data = 10;
+
+        //发送人民币给李四
+        pub_money->publish(money);
+        RCLCPP_INFO(this->get_logger(), "朕已阅：'%s'，打赏李四：%d 元的稿费", msg->data.c_str(), money.data);
+    };
+
+    //声明一个回调函数，当收到买书请求时调用该函数，用于处理数据
+    void sell_book_callback(const village_interfaces::srv::SellNovel::Request::SharedPtr request,
+        const village_interfaces::srv::SellNovel::Response::SharedPtr response)
+    {
+        //对请求数据进行处理
+    }
 
 };
 
